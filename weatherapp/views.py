3c2b5b7f1f640881e07ea3b613a1b0c2508b9b1c
yildirimkadir <yildirimkad34@gmail.com>
2022-09-13 21:13:06 +0200

    "views.py wurde aktualisiert."

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -5,62 +5,47 @@ from pprint import pprint
 from django.contrib import messages
 from .models import City
 
-
 def index(request):
-    API_KEY= config("API_KEY")
-    city="Dortmund"
-    user_city = request.POST.get("name")
-    # print(user_city)
-    if user_city:
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={API_KEY}&units=metric"
+    API_KEY = config("API_KEY")
+    city = "Erzurum"
+    u_city = request.POST.get("name")
+    
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
-        # print(response.ok)
+        print(response.ok)
         
         if response.ok:
-            pass
+            content = response.json()
+            r_city = content["name"]
+            if City.objects.filter(name=r_city):
+                messages.warning(request, "City already exists!")
+            else:
+                City.objects.create(name=r_city)
+            
         else:
             messages.warning(request, "There is no city")
+    city_data = []
+    cities = City.objects.all()
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        content = response.json()
+        data = {
+            "city" : content["name"],
+            "temp" : content["main"]["temp"],
+            "icon" : content["weather"][0]["icon"],
+            "desc" : content["weather"][0]["description"]
+        }
+        city_data.append(data)
+            
         
     
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    content = response.json()
-    # pprint(content)
-    # pprint(content["name"])
-    # pprint(content["main"]["temp"])
-    # pprint(content["weather"][0]["description"])
-    # pprint(content["weather"][0]["icon"])
     
+
+
     context = {
-        "city": content["name"],
-        "temp": content["main"]["temp"],
-        "icon": content["weather"][0]["icon"],
-        "desc": content["weather"][0]["description"]
+       "city_data" : city_data
     }
     
     return render(request, 'weatherapp/index.html', context)
-
-
-# {'base': 'stations',
-#  'clouds': {'all': 29},
-#  'cod': 200,
-#  'coord': {'lat': 39.5833, 'lon': 35.3333},
-#  'dt': 1663090364,
-#  'id': 296560,
-#  'main': {'feels_like': 288.12,
-#           'grnd_level': 883,
-#           'humidity': 56,
-#           'pressure': 1008,
-#           'sea_level': 1008,
-#           'temp': 289.02,
-#           'temp_max': 289.02,
-#           'temp_min': 289.02},
-#  'name': 'Yozgat Province',
-#  'sys': {'country': 'TR', 'sunrise': 1663039064, 'sunset': 1663084316},
-#  'timezone': 10800,
-#  'visibility': 10000,
-#  'weather': [{'description': 'scattered clouds',
-#               'icon': '03n',
-#               'id': 802,
-#               'main': 'Clouds'}],
-#  'wind': {'deg': 48, 'gust': 1.92, 'speed': 1.16}}
